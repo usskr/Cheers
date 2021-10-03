96efad9f55fb4225d89afb61c804bbdd5bd0034b
usskr <rei.takagi731@gmail.com>
2021-10-03 13:36:02 +0000

    フォロー・フォロワー機能のモデル、アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :cheers, dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :followings, through: :relationships, source: :followed
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_03_094635) do
+ActiveRecord::Schema.define(version: 2021_10_03_131639) do
 
   create_table "cheers", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2021_10_03_094635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
