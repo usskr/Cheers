3fe7ab385f30245fc2a1400c8a58573b4cd816aa
usskr <rei.takagi731@gmail.com>
2021-10-04 02:49:08 +0000

    フォロー・フォロワー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "/about" => "homes#about"
-  resources :users
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :posts do
     resources :post_comments, only: [:create, :destroy]
     resource :cheers, only: [:create, :destroy]
