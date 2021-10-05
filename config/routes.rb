26f4d2d6fe77f16afd65111990955cac081eafb3
usskr <rei.takagi731@gmail.com>
2021-10-05 05:11:22 +0000

    DM機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'chats/show'
   devise_for :users
   root to: 'homes#top'
   get "/about" => "homes#about"
@@ -11,4 +12,5 @@ Rails.application.routes.draw do
     resources :post_comments, only: [:create, :destroy]
     resource :cheers, only: [:create, :destroy]
   end
+  resources :chats, only: [:show, :create]
 end
