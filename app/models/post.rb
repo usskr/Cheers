5edfe1840ef9234369098b66f533225efef67835
usskr <rei.takagi731@gmail.com>
2021-10-06 13:43:04 +0000

    検索機能追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,15 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :cheers, dependent: :destroy
-  has_many :cheered_users, through: :cheers, source: :user
+  # has_many :cheered_users, through: :cheers, source: :user
 
   def cheered_by?(user)
     cheers.where(user_id: user.id).exists?
   end
 
   enum category: { 食事: 0, 運動: 1, おすすめ: 2, 失敗談: 3 }
+
+  def self.search(keyword)
+    where(["content like?", "%#{keyword}%"])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
-  get 'chats/show'
   devise_for :users
-  root to: 'homes#top'
+  root to: "homes#top"
   get "/about" => "homes#about"
   resources :users do
     resource :relationships, only: [:create, :destroy]
-    get 'followings' => 'relationships#followings', as: 'followings'
-    get 'followers' => 'relationships#followers', as: 'followers'
+    get "followings" => "relationships#followings", as: "followings"
+    get "followers" => "relationships#followers", as: "followers"
   end
   resources :posts do
     resources :post_comments, only: [:create, :destroy]
     resource :cheers, only: [:create, :destroy]
     collection do
-      get :rank
+      get :search
     end
+    # collection do
+    #   get :rank
+    # end
   end
   resources :chats, only: [:show, :create]
 end
