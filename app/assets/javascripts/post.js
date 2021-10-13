4e331eee5ed19d0dc6a28f5242dd03fc975e2013
usskr <rei.takagi731@gmail.com>
2021-10-13 10:06:40 +0000

    ユーザ情報編集画面のレイアウト

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,8 +1,17 @@
 //投稿画像のプレビュー
-$('#post_image').on('change', function (e) {
-  var reader = new FileReader();
-  reader.onload = function (e) {
-      $("#preview").attr('src', e.target.result);
+$('#post_image').on('change', function (e) { //post_imageというidを持つ場所に変化があったらfunction(e)を実行
+  var reader = new FileReader(); //readerを定義
+  reader.onload = function (e) { //ファイルの読み込み
+      $("#preview").attr('src', e.target.result); //idがpreviewになっているところに、画像データの取得結果を反映
   }
-  reader.readAsDataURL(e.target.files[0]);
+  reader.readAsDataURL(e.target.files[0]); //読み込まれた内容を実際に読み込む
+});
+
+//プロフィール画像のプレビュー
+$('#user_profile_image').on('change', function (e) { //post_imageというidを持つ場所に変化があったらfunction(e)を実行
+  var reader = new FileReader(); //readerを定義
+  reader.onload = function (e) { //ファイルの読み込み
+      $("#preview").attr('src', e.target.result); //idがpreviewになっているところに、画像データの取得結果を反映
+  }
+  reader.readAsDataURL(e.target.files[0]); //読み込まれた内容を実際に読み込む
 });
