4aae63a9d75d3e6239f3ccb5fe3e851b89584a3a
usskr <rei.takagi731@gmail.com>
2021-10-16 12:17:46 +0000

    検索機能修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,7 +12,13 @@ class Post < ApplicationRecord
   enum category: { 食事: 0, 運動: 1, おすすめ: 2, 失敗談: 3 }
 
   def self.search(keyword)
-    post_ids_belongs_to_spot = Spot.find_by(address: keyword).posts.ids
+    post_ids_belongs_to_spot = []
+    spots = Spot.where("address like ?", "%#{keyword}%")
+    if spots != nil
+      spots.each do |spot|
+        post_ids_belongs_to_spot.concat(spot.posts.ids) #concat配列を連結
+      end
+    end
     post_ids_includes_content = Post.where("content like ?", "%#{keyword}%").ids
     Post.where(id: [*post_ids_belongs_to_spot, *post_ids_includes_content])
   end
