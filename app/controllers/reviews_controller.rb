390b04caa8d8869dd2f54f3800695abfbcd831b1
usskr <rei.takagi731@gmail.com>
2021-10-16 04:57:16 +0000

    レビューフォームの部分テンプレート化

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,10 @@
 class ReviewsController < ApplicationController
   def create
-    @review = Review.new(review_params)
-    @spot = @review.spot
+    review = Review.new(review_params)
+    @spot = review.spot
     @reviews = @spot.reviews
-    @review.save
+    review.save
+    @review = Review.new #非同期通信でフォームを再読み込みする時に使う
   end
 
   private
