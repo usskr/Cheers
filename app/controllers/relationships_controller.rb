f0e80d77458837f4aa36bdb19174042f4d0ec0bc
usskr <rei.takagi731@gmail.com>
2021-10-24 06:52:07 +0000

    細部修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,21 +1,21 @@
 class RelationshipsController < ApplicationController
   def create
     current_user.follow(params[:user_id])
-    redirect_to request.referer
+    redirect_to request.referer, notice: "フォローしました"
   end
 
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    redirect_to request.referer, notice: "フォローを外しました"
   end
 
   def followings
-    user = User.find(params[:user_id])
-    @users = user.followings.where(is_valid: true)
+    @user = User.find(params[:user_id])
+    @users = @user.followings.where(is_valid: true)
   end
 
   def followers
-    user = User.find(params[:user_id])
-    @users = user.followers.where(is_valid: true)
+    @user = User.find(params[:user_id])
+    @users = @user.followers.where(is_valid: true)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,9 @@ class UsersController < ApplicationController
 
   def leave_page
     @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   def leave
