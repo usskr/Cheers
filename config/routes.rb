fe49ca8f2c67174dbdd2dff1159f1a2dd1e7bb01
usskr <rei.takagi731@gmail.com>
2021-10-31 04:58:40 +0000

    deviseの不要なページを非表示化

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:unlocks, :passwords, :mailer, :confimations]
   root to: "homes#top"
   resources :users do
     member do
